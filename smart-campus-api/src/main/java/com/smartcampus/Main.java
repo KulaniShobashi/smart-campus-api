cc8f84e3f88a4afa15b6d48ceda196fbe83bf6e3
KulaniShobashi <kulanishobashi@gmail.com>
2026-04-13 23:28:33 +0530

    create the packages and create main java file and embedded Tomcat server and connectets it to Jersey

--- smart-campus-api/src/main/java/com/smartcampus/Main.java
+++ smart-campus-api/src/main/java/com/smartcampus/Main.java
@@ -3,11 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.smartcampus;
+import com.smartcampus.config.ApplicationConfig;
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.glassfish.jersey.servlet.ServletContainer;
+
 
 /**
  *
  * @author kulanitennakoon
  */
+
 public class Main {
+    public static void main(String[] args) throws Exception {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        Context context = tomcat.addContext("", null);
+
+        Tomcat.addServlet(context, "jersey-servlet", new ServletContainer(new ApplicationConfig()));
+        context.addServletMappingDecoded("/*", "jersey-servlet");
+
+        tomcat.start();
+
+        System.out.println("Smart Campus API is running at:");
+        System.out.println("http://localhost:8080/api/v1/");
+        System.out.println("Press Ctrl + C to stop.");
+
+        tomcat.getServer().await();
+    }
     
 }
