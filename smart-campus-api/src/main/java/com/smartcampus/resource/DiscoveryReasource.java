2d16e884921ccb575bfef8de91205598ee14d1a5
KulaniShobashi <kulanishobashi@gmail.com>
2026-04-13 23:50:44 +0530

    Create DiscoveryResource java file

---
+++ smart-campus-api/src/main/java/com/smartcampus/resource/DiscoveryReasource.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author kulanitennakoon
+ */
+@Path("/")
+@Produce(MediaType.APPLICATION_JSON)
+public class DiscoveryReasource {
+    
+   @GET
+    public Map<String, Object> getApiInfo() {
+        Map<String, Object> response = new HashMap<>();
+        Map<String, String> resources = new HashMap<>();
+
+        resources.put("rooms", "/api/v1/rooms");
+        resources.put("sensors", "/api/v1/sensors");
+
+        response.put("name", "Smart Campus API");
+        response.put("version", "v1");
+        response.put("adminContact", "admin@smartcampus.local");
+        response.put("resources", resources);
+
+        return response;
+    } 
+    
+}
