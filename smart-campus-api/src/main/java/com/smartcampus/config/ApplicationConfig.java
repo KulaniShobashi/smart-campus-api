44410240380162d1eeea81cbfeaa154247022d07
KulaniShobashi <kulanishobashi@gmail.com>
2026-04-13 23:39:31 +0530

    create ApplicationConfig

---
+++ smart-campus-api/src/main/java/com/smartcampus/config/ApplicationConfig.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.config;
+
+import com.smartcampus.filter.LoggingFilter;
+import com.smartcampus.mapper.GlobalExceptionMapper;
+import com.smartcampus.resource.DiscoveryResource;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ *
+ * @author kulanitennakoon
+ */
+@ApplicationPath("/api/v1")
+public class ApplicationConfig {
+   
+    public ApplicationConfig() {
+        register(DiscoveryResource.class);
+        register(LoggingFilter.class);
+        register(GlobalExceptionMapper.class);
+
+        packages("com.smartcampus");
+    }
+
+    
+}
